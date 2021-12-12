042723fd0f184714aa67eeb41ee5468c9de7a6a9
Céryl Wiltink <ceryl.wiltink@trifork.nl>
2021-12-12 15:59:47 +0100

    Graphic dag 9 and dag 10 done

--- Sjerrul.AdventOfCode2021/Sjerrul.AdventOfCode2021/Day10/Day10Solver.cs
+++ Sjerrul.AdventOfCode2021/Sjerrul.AdventOfCode2021/Day10/Day10Solver.cs
@@ -17,14 +17,148 @@ namespace Sjerrul.AdventOfCode2021.Day1
 
         public async Task Part1()
         {
-            
-            Console.WriteLine($"Answer: ");
+            int total = 0;
+            foreach (var line in this.Input)
+            {
+                int points = 0;
+
+                Stack<char> stack = new Stack<char>();
+                foreach (var c in line)
+                {
+                    if (c == '[' || c == '(' || c == '<' || c == '{')
+                    {
+                        stack.Push(c);
+                        continue;
+                    }
+
+                    char topOfStack = stack.Pop();
+                    if (topOfStack == '[' && c != ']')
+                    {
+                        points += GetScore(c);
+                        break;
+                    }
+
+                    if (topOfStack == '<' && c != '>')
+                    {
+                        points += GetScore(c);
+                        break;
+                    }
+
+                    if (topOfStack == '{' && c != '}')
+                    {
+                        points += GetScore(c);
+                        break;
+                    }
+
+                    if (topOfStack == '(' && c != ')')
+                    {
+                        points += GetScore(c);
+                        break;
+                    }
+
+                }
+
+                total += points;
+            }
+
+            Console.WriteLine($"Answer: {total}");
         }
+        
+
 
         public async Task Part2()
         {
-            Console.WriteLine($"Answer: ");
+            int total = 0;
+            IList<string> incompleteLines = new List<string>();
+            foreach (var line in this.Input)
+            {
+                bool corrupted = false;
+
+                Stack<char> stack = new Stack<char>();
+                foreach (var c in line)
+                {
+                    if (c == '[' || c == '(' || c == '<' || c == '{')
+                    {
+                        stack.Push(c);
+                        continue;
+                    }
+
+                    char topOfStack = stack.Pop();
+                    if ((topOfStack == '[' && c != ']') || (topOfStack == '<' && c != '>') || (topOfStack == '{' && c != '}') || (topOfStack == '(' && c != ')'))
+                    {
+                        corrupted = true;
+                        break;
+                    }
+                }
+
+                if (!corrupted)
+                {
+                    incompleteLines.Add(line);
+                }
+            }
+
+            IList<long> scores = new List<long>();
+            foreach (var line in incompleteLines)
+            {
+                Stack<char> stack = new Stack<char>();
+                long score = 0;
+                foreach (var c in line)
+                {
+                    if (c == '[' || c == '(' || c == '<' || c == '{')
+                    {
+                        stack.Push(c);
+                        continue;
+                    }
+
+                    char topOfStack = stack.Pop();
+                }
+
+                while (stack.TryPop(out char openings))
+                {
+                    if (openings == '[')
+                    {
+                        score *= 5;
+                        score += 2;
+                    }
+
+                    if (openings == '<')
+                    {
+                        score *= 5;
+                        score += 4;
+                    }
+
+                    if (openings == '{')
+                    {
+                        score *= 5;
+                        score += 3;
+
+                    }
+
+                    if (openings == '(')
+                    {
+                        score *= 5;
+                        score += 1;
+
+                    }
+                }
+
+                scores.Add(score);
+            }
+
+            var orderedScores = scores.OrderBy(x => x);
+            Console.WriteLine($"Answer: {scores[scores.Count / 2]}");
         }
 
+        private int GetScore(char c)
+        {
+            switch (c)
+            {
+                case ')': return 3;
+                case ']': return 57;
+                case '}': return 1197;
+                case '>': return 25137;
+                default: throw new InvalidOperationException();
+            }
+        }
     }
 }
--- Sjerrul.AdventOfCode2021/Sjerrul.AdventOfCode2021/Day9/Day9Solver.cs
+++ Sjerrul.AdventOfCode2021/Sjerrul.AdventOfCode2021/Day9/Day9Solver.cs
@@ -17,17 +17,158 @@ namespace Sjerrul.AdventOfCode2021.Day1
 
         public async Task Part1()
         {
-            var matchingOutputs = this.Input.Select(line => line.Split(" | ")).Select(parts => parts[1]).Select(outputs => outputs.Split(' ')).Where(o => o.Length == 2 || o.Length == 3 || o.Length == 4 || o.Length == 9);
+            int[][] grid = new int[this.Input.Count() + 2][];
 
+            for (int i = 0; i < this.Input.Count(); i++)
+            {
+                string line = this.Input.ElementAt(i);
+                line = $"9{line}9";
 
-            Console.WriteLine($"Answer: {matchingOutputs.Count()}");
+                if (i == 0)
+                {
+                    grid[i] = new String('9', line.Length).Select(x => int.Parse(x.ToString())).ToArray();
+                }
+
+                grid[i + 1] = line.Select(x => int.Parse(x.ToString())).ToArray();
+
+                if (i == this.Input.Count() - 1)
+                {
+                    grid[i + 2] = new String('9', line.Length).Select(x => int.Parse(x.ToString())).ToArray();
+                }
+            }
+
+            int riskLevelSum = 0;
+            for (int y = 1; y < grid.GetLength(0) - 1; y++)
+            {
+                for (int x = 1; x < grid[y].GetLength(0) - 1; x++)
+                {
+                    int currentValue = grid[y][x];
+                    if (currentValue < grid[y - 1][x] && currentValue < grid[y + 1][x] && currentValue < grid[y][x - 1] && currentValue < grid[y][x + 1])
+                    {
+                        riskLevelSum += currentValue + 1;
+                        continue;
+                    }
+                }
+            }
+
+            Console.WriteLine($"Answer: {riskLevelSum}");
         }
+
 
         public async Task Part2()
         {
-          //  this.Input.Select(line => line.Split(" | ")).Select(parts => (parts[0], parts[1]))
-            Console.WriteLine($"Answer: ");
+            int[][] grid = new int[this.Input.Count() + 2][];
+
+            for (int i = 0; i < this.Input.Count(); i++)
+            {
+                string line = this.Input.ElementAt(i);
+                line = $"9{line}9";
+
+                if (i == 0)
+                {
+                    grid[i] = new String('9', line.Length).Select(x => int.Parse(x.ToString())).ToArray();
+                }
+
+                grid[i + 1] = line.Select(x => int.Parse(x.ToString())).ToArray();
+
+                if (i == this.Input.Count() - 1)
+                {
+                    grid[i + 2] = new String('9', line.Length).Select(x => int.Parse(x.ToString())).ToArray();
+                }
+            }
+
+            IList<(int, int)> lowestPpoints = new List<(int, int)>();
+            for (int y = 1; y < grid.GetLength(0) - 1; y++)
+            {
+                for (int x = 1; x < grid[y].GetLength(0) - 1; x++)
+                {
+                    int currentValue = grid[y][x];
+                    if (currentValue < grid[y - 1][x] && currentValue < grid[y + 1][x] && currentValue < grid[y][x - 1] && currentValue < grid[y][x + 1])
+                    {
+                        lowestPpoints.Add((x, y));
+                        continue;
+                    }
+                }
+            }
+            IList<int> basinSizes = new List<int>();
+            foreach (var start in lowestPpoints)
+            {
+                var result = Fill(grid, start.Item1, start.Item2);
+                foreach (var point in result)
+                {
+                    RenderPoint(point.Item1, point.Item2, '0');
+                    await Task.Delay(1);
+                }
+                basinSizes.Add(result.Count);
+            }
+
+
+            //IList<int> basinSizes = new List<int>();
+            //IEnumerable<(int, int)> visited = new List<(int, int)>();
+            //for (int y = 1; y < grid.GetLength(0) - 1; y++)
+            //{
+            //    for (int x = 1; x < grid[y].GetLength(0) - 1; x++)
+            //    {
+            //        if (visited.Contains((x, y)))
+            //        {
+            //            continue;
+            //        }
+
+            //        if (grid[y][x] == 9)
+            //        {
+            //            continue;
+            //        };
+
+            //        var result = Fill(grid, x, y);
+            //        visited = visited.Concat(result.Distinct());
+
+            //        foreach (var point in visited)
+            //        {
+            //            RenderPoint(point.Item1, point.Item2, 'X');
+            //            await Task.Delay(1);
+            //        }
+            //        basinSizes.Add(result.Count);
+            //    }
+            //}
+
+            var biggest = basinSizes.OrderByDescending(x => x).Take(3);
+            Console.WriteLine($"Answer: {biggest.ElementAt(0) * biggest.ElementAt(1) * biggest.ElementAt(2)}");
         }
 
+        public IList<(int, int)> Fill(int[][] array, int x, int y)
+        {
+            Queue<(int, int)> queue = new Queue<(int, int)>();
+            queue.Enqueue((x, y));
+
+            IList<(int, int)> list = new List<(int, int)>();
+            while (queue.Any())
+            {
+                (int, int) point = queue.Dequeue();
+                if (!list.Contains(point))
+                {
+                    list.Add(point);
+
+                    EnqueueIfMatches(array, queue, point.Item1 - 1, point.Item2, list);
+                    EnqueueIfMatches(array, queue, point.Item1 + 1, point.Item2, list);
+                    EnqueueIfMatches(array, queue, point.Item1, point.Item2 - 1, list);
+                    EnqueueIfMatches(array, queue, point.Item1, point.Item2 + 1, list);
+                }
+               
+            }
+
+            return list;
+        }
+
+        private void EnqueueIfMatches(int[][] array, Queue<(int, int)> queue, int x, int y, IList<(int, int)> visited)
+        {
+            if (array[y][x] != 9 && !visited.Contains((x, y)))
+                queue.Enqueue((x, y));
+        }
+
+        private void RenderPoint(int x, int y, char c)
+        {
+            Console.SetCursorPosition(x, y);
+            Console.Write(c);
+        }
     }
 }
